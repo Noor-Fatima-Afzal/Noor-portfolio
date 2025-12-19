56ec7fa247a85d7bc8504bea2fc75f782fd249ff
Noor-Fatima-Afzal <noorfatimaafzabutt@gmai.com>
2025-12-19 18:47:16 +0500

    modifications

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Awards from "./pages/Awards";
 import Projects from "./pages/Projects";
@@ -15,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/Noor-portfolio/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/awards" element={<Awards />} />
