8906e8668887ab3886e608c430aa2f3c2b394366
Noor-Fatima-Afzal <noorfatimaafzabutt@gmai.com>
2025-12-19 18:41:12 +0500

    modifications

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Noor-portfolio/' 
-})
+  base: '/Noor-portfolio/', // for GitHub Pages
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // <--- This allows @ to point to src
+    },
+  },
+});
